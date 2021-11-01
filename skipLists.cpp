8ffd950f0aca08b735d235bbedafc049d8e7a9b7
xiaoyong zhao <xiaoyong.zhao@dolphindb.com>
2021-11-01 10:52:39 +0800

    single thread skiplist

--- skipLists.cpp
+++ skipLists.cpp
@@ -1,20 +1,32 @@
 #include <mutex>
 #include <random>
 #include <utility>
+#include <cstring>
 #include "codec.h"
-#define SKIPLIST_MAX_HEIGHT 64
-#define SKIPLIST_P 0.25 /* Skiplist P = 1/4 */
+#define SKIPLIST_MAX_HEIGHT 12
 template<typename K, typename V>
 struct SkipNode {
     SkipNode(double score, int height, Entry<K, V>&& elem): score_(score), elem_(std::forward<Entry<K, V>>(elem)), height_(height) {
-        nexts_ = std::make_unique<std::shared_ptr<SkipNode<K, V>>[]>(height_ + 1);
+        nexts_ = new SkipNode<K, V>*[height_ + 1];
+        for (int i = 0; i < height_ + 1; i++) {
+            nexts_[i] = nullptr;
+        }
     }
     
     SkipNode(double score, int height): score_(score), height_(height){
-        nexts_ = std::make_unique<std::shared_ptr<SkipNode<K, V>>[]>(height_ + 1);
+        nexts_ = new SkipNode<K, V>*[height_ + 1];
+        for (int i = 0; i < height_ + 1; i++) {
+            nexts_[i] = nullptr;
+        }
     }
 
     ~SkipNode() {
+        if (nexts_[0]) {
+            delete nexts_[0];
+            nexts_[0] = nullptr;
+        }
+        delete[] nexts_;
+        nexts_ = nullptr;
     }
 
     void update(Entry<K, V>&& elem) {
@@ -25,7 +37,7 @@ struct SkipNode {
     double score_;
     Entry<K, V> elem_;
     
-    std::unique_ptr<std::shared_ptr<SkipNode<K, V>>[]> nexts_;
+    SkipNode<K, V>** nexts_;
     int height_;
 };
 
@@ -45,35 +57,35 @@ double CalculateKeyScore(const char* key) {
     }
     uint64_t hash = 0;
     for (int i = 0; i < len; i++) {
-        uint64_t shift = uint64_t(64 - 8 - i * 8);
+        int shift = 64 - 8 - i * 8;
         hash |= uint64_t(key[i]) << shift;
     }
     return double(hash);
 }
 
-double CalculateKeyScore(const std::string& key) {
-    size_t len = key.size();
-    if (len > 8) {
-        len = 8;
-    }
-    uint64_t hash;
-    for (size_t i = 0; i < len; i++) {
-        uint64_t shift = uint64_t(64 - 8 - i * 8);
-        hash |= uint64_t(key[i]) << shift;
-    }
-    return double(hash);
-}
+// double CalculateKeyScore(const std::string& key) {
+//     size_t len = key.size();
+//     if (len > 8) {
+//         len = 8;
+//     }
+//     uint64_t hash;
+//     for (size_t i = 0; i < len; i++) {
+//         uint64_t shift = uint64_t(64 - 8 - i * 8);
+//         hash |= uint64_t(key[i]) << shift;
+//     }
+//     return double(hash);
+// }
 
 // template<typename K>
 // int CompareKey(const K& key1, const K& key2) {
 //     return 0;
 // }
 
-int CompareKey(const TestMove& key1, const TestMove& key2) {
+inline int CompareKey(const TestMove& key1, const TestMove& key2) {
     return key1.a_ < key2.a_;
 }
 
-int CompareKey(const char* key1, const char* key2) {
+inline int CompareKey(const char* key1, const char* key2) {
     return strcmp(key1, key2);
 }
 
@@ -82,17 +94,25 @@ class SkipList {
 public:
     SkipList() {
         max_height_ = 0;
-        header_ = std::make_shared<SkipNode<K, V>>(-1, SKIPLIST_MAX_HEIGHT);
-        prev_ = std::make_unique<std::shared_ptr<SkipNode<K, V>>[]>(SKIPLIST_MAX_HEIGHT);
+        header_ = new SkipNode<K, V>(-1, SKIPLIST_MAX_HEIGHT);
+        prev_ = new SkipNode<K, V>*[SKIPLIST_MAX_HEIGHT];
     }
 
     ~SkipList() {
+        if (header_->nexts_[0]) {
+            delete header_->nexts_[0];
+            header_->nexts_[0] = nullptr;
+        }
+        delete header_;
+        header_ = nullptr;
+        delete[] prev_;
+        prev_ = nullptr;
     }
 
 
     int RandomHeight() {
         int level = 0;
-        while ((random()&0xFFFF) < (SKIPLIST_P * 0xFFFF))
+        while (random() % 4 == 0)
             level += 1;
         return (level < SKIPLIST_MAX_HEIGHT) ? level : SKIPLIST_MAX_HEIGHT;
     }
@@ -102,7 +122,7 @@ public:
         return CalculateKeyScore(elem.key_);
     }
 
-    int Compare(std::shared_ptr<SkipNode<K, V>> node, const K& key, double score) const {
+    inline int Compare(SkipNode<K, V>* node, const K& key, double score) const {
         if (node->score_ > score) {
             return 1;
         } else if (node->score_ < score) {
@@ -114,10 +134,10 @@ public:
 
     const Entry<K, V>* Contains(const K& key) const {
         double key_score = CalculateKeyScore(key);
-        std::shared_ptr<SkipNode<K, V>> cur_node = header_;
+        SkipNode<K, V>* cur_node = header_;
         int cur_height = max_height_;
         while (cur_height >= 0 && cur_node != nullptr) {
-            std::shared_ptr<SkipNode<K, V>> next_node = cur_node->nexts_[cur_height];
+            SkipNode<K, V>* next_node = cur_node->nexts_[cur_height];
             if (next_node != nullptr && Compare(next_node, key, key_score) < 0) {
                 cur_node = next_node;
             } else {
@@ -138,10 +158,10 @@ public:
     void Insert(Entry<K, V>&& elem) {
         double key_score = CalculateScore(elem);
         // std::cout << "score:" << key_score << std::endl;
-        std::shared_ptr<SkipNode<K, V>> cur_node = header_;
+        SkipNode<K, V>* cur_node = header_;
         int cur_height = max_height_;
         while (cur_height >= 0 && cur_node != nullptr) {
-            std::shared_ptr<SkipNode<K, V>> next_node = cur_node->nexts_[cur_height];
+            SkipNode<K, V>* next_node = cur_node->nexts_[cur_height];
             if (next_node != nullptr && Compare(next_node, elem.key_, key_score) < 0) {
                 cur_node = next_node;
             } else {
@@ -162,8 +182,9 @@ public:
         for (int i = max_height_ + 1; i <= random_height; i++) {
             prev_[i] = header_;
         }
+        max_height_ = std::max(random_height, max_height_);
         
-        std::shared_ptr<SkipNode<K, V>> skipnode = std::make_shared<SkipNode<K, V>>(key_score, random_height, std::forward<Entry<K, V>>(elem));
+        SkipNode<K, V>* skipnode = new SkipNode<K, V>(key_score, random_height, std::forward<Entry<K, V>>(elem));
         for (int i = 0; i <= random_height; i++) {
             skipnode->nexts_[i] = prev_[i]->nexts_[i];
         }
@@ -172,11 +193,10 @@ public:
             prev_[i]->nexts_[i] = skipnode;
         }
         
-        
     }
 
 private:
-    std::shared_ptr<SkipNode<K, V>> header_;
-    std::unique_ptr<std::shared_ptr<SkipNode<K, V>>[]> prev_;    
+    SkipNode<K, V>* header_;
+    SkipNode<K, V>** prev_;    
     int max_height_;
 };
--- skipListsTest.cpp
+++ skipListsTest.cpp
@@ -20,61 +20,61 @@ std::string RandString(int len) {
     return std::string(bytes);
 }
 
-// TEST(SkipTest, TestMove) {
-//     TestMove a(1.2, 1);
-//     TestMove b(2.3, 1);
-//     TestMove c(1.2, 2);
+TEST(SkipTest, TestMove) {
+    TestMove a(1.2, 1);
+    TestMove b(2.3, 1);
+    TestMove c(1.2, 2);
 
-//     std::cout << "generate" << std::endl;
-//     Entry<TestMove, TestMove> entry1(std::move(a), std::move(b));
+    std::cout << "generate" << std::endl;
+    Entry<TestMove, TestMove> entry1(std::move(a), std::move(b));
 
-//     SkipList<TestMove, TestMove> skipList;
+    SkipList<TestMove, TestMove> skipList;
 
 
-//     ASSERT_EQ(skipList.Contains(c), nullptr);
+    ASSERT_EQ(skipList.Contains(c), nullptr);
 
-//     skipList.Insert(std::move(entry1));
+    skipList.Insert(std::move(entry1));
 
-//     TestMove d(2.3, 1);
+    TestMove d(2.3, 1);
 
-//     ASSERT_NE(skipList.Contains(a), nullptr);
-//     ASSERT_EQ(skipList.Contains(a)->value_, d);
+    ASSERT_NE(skipList.Contains(a), nullptr);
+    ASSERT_EQ(skipList.Contains(a)->value_, d);
 
-//     TestMove e(1.2, 1);
-//     TestMove f(2.111, 10);
+    TestMove e(1.2, 1);
+    TestMove f(2.111, 10);
 
-//     Entry<TestMove, TestMove> entry2(std::move(e), std::move(f));
-//     std::cout << "Update before" << std::endl;
-//     skipList.Insert(std::move(entry2));
-//     std::cout << "Update After" << std::endl;
+    Entry<TestMove, TestMove> entry2(std::move(e), std::move(f));
+    std::cout << "Update before" << std::endl;
+    skipList.Insert(std::move(entry2));
+    std::cout << "Update After" << std::endl;
     
-// }
+}
 
-// TEST(SkipTest, BasicCRUD) {
-//     SkipList<char*, char*> skipList;
+TEST(SkipTest, BasicCRUD) {
+    SkipList<char*, char*> skipList;
     
-//     Entry<char*, char*> entry1((char*)"key1", (char*)"value1");
-//     ASSERT_EQ(skipList.Contains((char*)"key2"), nullptr);
-//     skipList.Insert(std::move(entry1));
-//     ASSERT_NE(skipList.Contains((char*)"key1"), nullptr);
-//     ASSERT_EQ(skipList.Contains((char*)"key1")->value_, (char*)"value1");
+    Entry<char*, char*> entry1((char*)"key1", (char*)"value1");
+    ASSERT_EQ(skipList.Contains((char*)"key2"), nullptr);
+    skipList.Insert(std::move(entry1));
+    ASSERT_NE(skipList.Contains((char*)"key1"), nullptr);
+    ASSERT_EQ(skipList.Contains((char*)"key1")->value_, (char*)"value1");
 
-//     Entry<char*, char*> entry2((char*)"key2", (char*)"value2");
-//     skipList.Insert(std::move(entry2));
-//     ASSERT_NE(skipList.Contains((char*)"key2"), nullptr);
-//     ASSERT_EQ(skipList.Contains((char*)"key2")->value_, (char*)"value2");
+    Entry<char*, char*> entry2((char*)"key2", (char*)"value2");
+    skipList.Insert(std::move(entry2));
+    ASSERT_NE(skipList.Contains((char*)"key2"), nullptr);
+    ASSERT_EQ(skipList.Contains((char*)"key2")->value_, (char*)"value2");
 
-//     ASSERT_EQ(skipList.Contains((char*)"key3"), nullptr);
+    ASSERT_EQ(skipList.Contains((char*)"key3"), nullptr);
 
-//     Entry<char*, char*> entry3((char*)"key1", (char*)"value1 + value2");
-//     skipList.Insert(std::move(entry3));
-//     ASSERT_NE(skipList.Contains((char*)"key1"), nullptr);
-//     ASSERT_EQ(skipList.Contains((char*)"key1")->value_, (char*)"value1 + value2");
-// }
+    Entry<char*, char*> entry3((char*)"key1", (char*)"value1 + value2");
+    skipList.Insert(std::move(entry3));
+    ASSERT_NE(skipList.Contains((char*)"key1"), nullptr);
+    ASSERT_EQ(skipList.Contains((char*)"key1")->value_, (char*)"value1 + value2");
+}
 
 TEST(SkipTest, BenchmarkCRUD) {
     SkipList<char*, char*> skipList;
-    int maxTime = 10000;
+    int maxTime = 100000;
     int maxLen = 1000;
     for (int i = 0; i < maxTime; i++) {
         char key[maxLen];
